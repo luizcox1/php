f66f0408e3b6bd24b859598d2b37186ff30fc67b
LUIZ <luizcox1@hotmail.com>
2019-01-02 13:16:04 -0200

    traduzi o texto

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
 
-$arrasy = new array();
-
-$arrasy[0] ="laranja";
-$arrasy[1] ="abacaxi";
-$arrasy[2] ="acerola";
-$arrasy[3] ="manga";
-$arrasy[4] ="maracujá";
-
-
-var_dump($arrasy);
-
+echo "ola mundo";
 ?>
 
