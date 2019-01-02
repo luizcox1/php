8410795d13e5f1162fed443392b3e2fc4560e7a8
luiz pinheiro <luizcox1@hotmail.com>
2019-01-02 12:55:57 -0200

    inicio curso php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$arrasy = new array();
+
+$arrasy[0] ="laranja";
+$arrasy[1] ="abacaxi";
+$arrasy[2] ="acerola";
+$arrasy[3] ="manga";
+$arrasy[4] ="maracujá";
+
+
+var_dump($arrasy);
+
+?>
+
