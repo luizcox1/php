7ba3927a533d3d7880c199c3030a8dfa4598655c
LUIZ <luizcox1@hotmail.com>
2019-01-03 12:44:19 -0200

    atualizado

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-echo "Olá mundo 456!";
-echo "Olá Udemy!";
+echo "Olá mundo 123!";
+echo "Olá hcode!";
 
-=======
-echo "ola mundo 123";
-
-echo "olá hcode"
->>>>>>> fa8415f38721866241db0601697c94d8a5355a34
-?>
 
 
